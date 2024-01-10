75d57598be9e9274048872b91d4548e8170de6db
physwizz <pollockg86@gmail.com>
2024-01-10 12:04:57 +1100

    first

---
+++ twrp_m53x.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit from m53x device
+$(call inherit-product, device/samsung/m53x/device.mk)
+
+PRODUCT_DEVICE := m53x
+PRODUCT_NAME := twrp_m53x
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-M536B
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="m53xnsxx-user 12 SP1A.210812.016 M536BXXU3CWF2 release-keys"
+
+BUILD_FINGERPRINT := samsung/m53xnsxx/m53x:12/SP1A.210812.016/M536BXXU3CWF2:user/release-keys
